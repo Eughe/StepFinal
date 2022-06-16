0800c906124dcbd0ec9c76da31a75cc1632a4db7
Eughe <103205325+Eughe@users.noreply.github.com>
2022-06-16 20:20:15 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let iconMenu = document.querySelector('.icon');
+let menuBody = document.querySelector('.menu');
+
+if (iconMenu){
+    iconMenu.addEventListener('click', function(e){
+        iconMenu.classList.toggle('active');
+        menuBody.classList.toggle('active');
+    })
+}
